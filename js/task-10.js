04c95247225ef516bc3b6fe77671523b001ac06a
Yana Lapach <y.lapach@gmail.com>
2023-03-01 00:15:06 +0100

    task - 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector('#boxes');
+const controls = document.querySelector('#controls');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const input = controls.querySelector('input');
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${30 + i * 10}px`;
+    divEl.style.height = `${30 + i * 10}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    document.querySelector('#boxes').appendChild(divEl);
+  }
+}
+
+function destroyBoxes() {
+    boxes.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
