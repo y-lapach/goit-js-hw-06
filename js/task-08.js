1f0c629d1cd0173b3445d8b5d362b121e9284abf
Yana Lapach <y.lapach@gmail.com>
2023-02-28 23:11:48 +0100

    task - 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    const inputData = {
+        Email: `${email.value}`,
+        Password: `${password.value}`
+    }
+    console.log(inputData)
+    
+    if (email.value === "" || password.value === "") {
+        return window.alert("Please fill in all fields!");
+    }
+   event.currentTarget.reset();
+});
