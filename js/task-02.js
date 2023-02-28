dda62dd7b8f4a622d20934dc9084d0fbf947416b
Yana Lapach <y.lapach@gmail.com>
2023-02-28 21:10:22 +0100

    task - 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = ingredient;
+  return listItem;
+});
+console.log(items);
+
+ingredientsList.append(...items);
